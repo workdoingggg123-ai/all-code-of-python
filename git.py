da7cbee534981f2319b7e76f5c31ef8963376a25
workdoingggg123-ai <workdoingggg123@gmail.com>
2026-03-04 17:12:06 +0530

    substraction

--- git.py
+++ git.py
@@ -2,9 +2,11 @@ class calculator:
 
     def addition(self , a,b):
         return print(a + b)
-    
+    def substraction(self ,a ,b):
+        return print(a-b)
     
 
 
 mini_calci = calculator()
+mini_calci.substraction(3,6)
 mini_calci.addition(3,4)    
